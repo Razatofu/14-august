7f1bbcf091541e78f048ab52a12be1e387979a25
raza ayaz <razaayaz@yahoo.com>
2025-02-03 09:35:23 +0500

    Add files via upload

---
+++ abc.js
@@ -0,0 +1,16 @@
+
+
+
+
+let currentYear = new Date().getFullYear();
+
+
+for (let year = currentYear; year < currentYear + 50; year++) {
+  let AugInd = new Date(year, 8, 14); 
+  
+  
+  if (AugInd.getDay() === 0) {
+    console.log(`14 August Day on Sunday in ${year}: ${AugInd.toDateString()}`);
+  }
+}
+
